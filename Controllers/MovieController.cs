a0ee2236d0dfaadfeedb2dc7bebe9c19fa73ede3
Edson Carlos <edsoncarlos@ymail.com>
2024-01-16 19:19:48 -0300

    feat: Método post criado

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using api_movie.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_movie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<MovieModel> movies = new List<MovieModel>();
+
+        [HttpPost]
+        public void AddMovie([FromBody] MovieModel movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+            Console.WriteLine(movie.Genere);
+            Console.WriteLine(movie.Duration);
+        }
+    }
+}
---
+++ Models/MovieModel.cs
@@ -0,0 +1,9 @@
+﻿namespace api_movie.Models
+{
+    public class MovieModel
+    {
+        public String Title { get; set; }
+        public String Genere { get; set; }
+        public int Duration { get; set; }
+    }
+}
