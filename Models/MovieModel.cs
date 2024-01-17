5d66de3b424eb583bdd9022333c6656c6c92aaec
Edson Carlos <edsoncarlos@ymail.com>
2024-01-17 00:38:16 -0300

    feat: Inserindo validação dos campos

--- Models/MovieModel.cs
+++ Models/MovieModel.cs
@@ -1,9 +1,17 @@
-﻿namespace api_movie.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_movie.Models
 {
     public class MovieModel
     {
-        public String Title { get; set; }
-        public String Genere { get; set; }
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public String? Title { get; set; }
+
+        [Required(ErrorMessage = "O genero do filme é obrigatório")]
+        public String? Genere { get; set; }
+
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração do filme deve estar entre 70 e 600 minutos")]
         public int Duration { get; set; }
     }
 }
